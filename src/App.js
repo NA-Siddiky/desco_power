b35a260ea0894b5e542f954ff44f59736d09e198
na-siddiky <siddiky.academic@gmail.com>
2022-01-07 20:56:15 +0600

    firebase auth done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import initializeAuthentication from "./Firebase/firebase.initialize";
 import View from "./View";
+
+initializeAuthentication();
+
+const provider = new GoogleAuthProvider();
+
+
 function App() {
+  const handleGoogleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+  }
   return (
     <>
+      <button onClick={handleGoogleSignIn}>google sign in</button>
       <View />
       <ToastContainer />
     </>
