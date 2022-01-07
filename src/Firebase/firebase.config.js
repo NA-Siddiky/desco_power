830b37e74b2979124d6418165c7a6c9cb1caae9f
na-siddiky <siddiky.academic@gmail.com>
2022-01-07 20:36:26 +0600

    firebase auth

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCBrptrTxmTHqnSa475XQU8TVXUDzFgR1I",
+    authDomain: "desco-power.firebaseapp.com",
+    projectId: "desco-power",
+    storageBucket: "desco-power.appspot.com",
+    messagingSenderId: "272514542103",
+    appId: "1:272514542103:web:d34a16921f2b9f49300a52"
+};
+
+export default firebaseConfig;
